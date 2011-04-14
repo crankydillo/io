c0a6411b3adb39b3a3411831cbcd52446502d25d
Samuel Cox <crankydill@gmail.com>
2011-04-13 21:43:01 -0500

    Add write.

--- src/main/scala/org/beeherd/io/FileUtil.scala
+++ src/main/scala/org/beeherd/io/FileUtil.scala
@@ -1,10 +1,12 @@
 package org.beeherd.io
 
-import java.io.File
+import java.io.{File, FileOutputStream}
 
 object FileUtil {
   /** 
    * Delete the file whether it is a directory or a file.
+   * <p>
+   * This uses recursion.  Deep directories might overflow the stack.
    */
   def delete(f: File): Unit = {
    // For now, I'm going to do some stuff commons-io will do so that the
@@ -14,6 +16,26 @@ object FileUtil {
     else {
       f.listFiles.foreach {delete _}
       f.delete // now we have to worry about the stack..
+    }
+  }
+
+  /**
+   * Use some file for writing.  Clean is handled.
+   */
+   def write[T](f: File)(fun: FileOutputStream => T): T = {
+    // I saw some Odersky speech where he had this concept applied to anything
+    // that provided a close() method, which probably relies on reflection...
+    val out = new FileOutputStream(f);
+    try {
+      fun(out);
+    } finally {
+      if (out != null) {
+        try {
+          out.close()
+        } catch {
+          case e:Exception => {/* TODO Log?? */}
+        }
+      }
     }
   }
 }
